34d491c4926edad6e917f6361c95964b4fc7e9dd
GrigorMarinov <114864279+GrigorMarinov@users.noreply.github.com>
2022-10-07 17:42:40 +0300

    Add files via upload

---
+++ Programming Basics/C++ Programming Basics/29.09.2022/Задача 2/Задача 2.cpp
@@ -0,0 +1,25 @@
+﻿// Задача 2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "o o o o o o";
+	cout << endl;
+	cout << "o";
+	cout << endl;
+	cout << "o";
+	cout << endl;
+	cout << "o";
+	cout << endl;
+	cout << "o";
+	cout << endl;
+	cout << "o";
+	cout << endl;
+	cout << "o";
+	cout << endl;
+	return 0;
+}
+
---
+++ Programming Basics/C++ Programming Basics/29.09.2022/Задача 4/Задача 4.cpp
@@ -0,0 +1,17 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a = 12;
+	int b = 8;
+	int p = 2 * a + 2 * b;
+	int s = a * b;
+	cout << "The perimeter of rectangle is: " << p;
+	cout << endl;
+	cout << "The area of rectangle is: " << s;
+	return 0;
+}
+
+
