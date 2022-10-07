de01da2e728834c5ccae2aed68a100175840a0ad
GrigorMarinov <114864279+GrigorMarinov@users.noreply.github.com>
2022-10-07 17:40:58 +0300

    Add files via upload

---
+++ Programming Basics/C++ Programming Basics/06.10.2022/Задача 3/Задача 3.cpp
@@ -0,0 +1,22 @@
+﻿// Програма която прочита от конзолата страните a и b на правоъгълник.
+// Пресмета периметъра и лицето и ги извежда на екрана
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double a, b, perimeter, area;
+    cout << "Enter the size of a: ";
+    cin >> a;
+    cout << "Enter the size of b: ";
+    cin >> b;
+    perimeter = (2 * a) + (2 * b);
+    area = a * b;
+    cout << "The perimeter of rectangle is: " << perimeter;
+    cout << endl;
+    cout << "The area of rectangle is: " << area;
+    return 0;
+}
+
+
